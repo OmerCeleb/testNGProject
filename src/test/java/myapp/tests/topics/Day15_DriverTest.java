e197f3c189a32e45183a2600cb17aebc5b219adc
OmerCeleb <omer534@outlook.com>
2024-02-19 20:30:57 +0100

    Day02_testNgProject

--- src/test/java/myapp/tests/topics/Day15_DriverTest.java
+++ src/test/java/myapp/tests/topics/Day15_DriverTest.java
@@ -1,4 +1,28 @@
 package myapp.tests.topics;
 
+import myapp.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Day15_DriverTest {
+
+    @Test
+    public void driverTest() throws InterruptedException {
+
+        //    driver.get("https://www.amazon.se"); We were calling driver from TestBase class in JUnit framework
+
+        Driver.getDriver().get("https://www.amazon.se");
+        Thread.sleep(2000);
+        Driver.getDriver().navigate().refresh(); // to get rid of captcha
+
+        String title = Driver.getDriver().getTitle();
+        System.out.println("title = " + title);
+        Assert.assertTrue(title.contains("Amazon.se"));
+
+        // To close the driver
+        Driver.closeDriver();
+
+
+    }
+
 }
