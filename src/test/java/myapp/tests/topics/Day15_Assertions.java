47afe160c34fa10497ce999ef785eaf983604c48
OmerCeleb <omer534@outlook.com>
2024-02-19 15:57:26 +0100

    Day02_testNgProject

---
+++ src/test/java/myapp/tests/topics/Day15_Assertions.java
@@ -0,0 +1,79 @@
+package myapp.tests.topics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import static org.testng.Assert.assertTrue;
+
+public class Day15_Assertions {
+
+    /*
+
+    In JUnit, we had only HARD ASSERTION
+    In TestNG, we have two types of Assertions ==> Hard and Soft
+    HARD ASSERTION works the same in TestNG as it works in JUnit. it stops executing rest of the test
+    if assertion fails at any point. We get the error message / exception straight away
+
+    SOFT ASSERTION is different in a sense that it doesn't stop executing the test if it at fails at any point
+    It continues executing the test and informs in the end which step the test failed at. Only IF WE USE assertAll() is
+    mandatory last step.
+
+     */
+
+    @Test
+    public void assertionsTest() {
+
+        System.out.println("Checking how hard assertion works");
+        assertTrue(true); // passes
+        System.out.println("Line 17");
+        assertTrue(false); // fails at this point... so the rest of the test is not executed
+        System.out.println("Line 19");
+
+    }
+
+    @Test
+    public void javaAssert() {
+
+        /*
+        This comes from Java Library. It works the same way as HARD ASSERTION
+        But we (testers) prefer TestNG assertions, specially HARD ASSERTION
+         */
+
+        System.out.println("Line 42");
+        assert 3 < 5;
+        System.out.println("Line 44");
+        assert "Apple".toLowerCase().contains("c");
+        System.out.println("Line 46");
+        assert "apple" == "apple";
+        System.out.println("Line 48");
+
+    }
+
+    @Test
+    public void softAssert() {
+
+        /*
+        If Soft Assertion fails at any step. it continues to execute the test steps
+        1. Create Soft Assertion object
+        2. Use the Soft Assertion method such as assertEquals(), assertTrue() etc.
+        3. Use assertAll() as a last step, otherwise you cannot find out where if the test step failed
+         */
+
+        System.out.println("Line 63");
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals("5", 5);
+        System.out.println("Line 67");
+        softAssert.assertTrue("Apple".toLowerCase().contains("a"));
+        System.out.println("Line 69");
+        softAssert.assertTrue("apple".equals("orange"));
+        System.out.println("Line 71");
+
+        softAssert.assertEquals("pineapple", "grapes");
+        System.out.println("Line 74");
+        softAssert.assertAll();
+
+    }
+
+}
---
+++ src/test/java/myapp/tests/topics/Day15_DependsOnGroups.java
@@ -0,0 +1,39 @@
+package myapp.tests.topics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day15_DependsOnGroups {
+
+
+    @Test(dependsOnGroups = "Smoke Test")
+    // groups keyboard is used to declare that this test belongs to a particular group
+    public void homePageTest() {
+        System.out.println("Home page test...");
+        Assert.assertTrue(false);  // test fails at this step
+
+    }
+
+
+    @Test(groups = "Smoke Test")
+    public void searchTest() {
+        System.out.println("Searching something ...");
+        Assert.assertTrue(false);  // test fails at this step
+
+    }
+
+    @Test(groups = "Regression Test")
+    public void checkOutTest() {
+        System.out.println("User is checking out...");
+    }
+
+    @Test(dependsOnGroups = "Smoke Test")
+    public void logOutTest() {
+        System.out.println("Logging out...");
+    }
+
+    // logOutTest is dependent on the group called "Smoke Test", if all tests belonging to this group pass, logOutTest will pass to
+    // otherwise it will be skipped
+
+
+}
