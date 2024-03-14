3ed995a3e16ba4653e7d8668dbdc0d25856b9923
OmerCeleb <omer534@outlook.com>
2024-03-14 19:41:20 +0100

    Day07_testNgProject

--- src/test/java/myapp/pages/Medunna_HomePage.java
+++ src/test/java/myapp/pages/Medunna_HomePage.java
@@ -1,6 +1,21 @@
 package myapp.pages;
 
+import myapp.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Medunna_HomePage {
+
+    public Medunna_HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = ("account-menu"))
+    public WebElement userIcon;
+
+
 
 
 
