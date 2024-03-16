1d01a9e1bf28c7e527259ac5c072bc91574a50e9
OmerCeleb <omer534@outlook.com>
2024-03-16 16:59:50 +0100

    Day07_testNgProject

--- src/test/java/myapp/pages/Medunna_RegistrationPage.java
+++ src/test/java/myapp/pages/Medunna_RegistrationPage.java
@@ -1,6 +1,8 @@
 package myapp.pages;
 
 import myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Medunna_RegistrationPage {
@@ -8,6 +10,13 @@ public class Medunna_RegistrationPage {
     public Medunna_RegistrationPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(id = "ssn")
+    public WebElement ssn;
+
+    @FindBy(xpath = "//div[text()='Your SSN is invalid']")
+    public WebElement invalidSsnMessage;
+
 
 
 }
--- src/test/java/myapp/tests/medunna_practice/Day21_US001.java
+++ src/test/java/myapp/tests/medunna_practice/Day21_US001.java
@@ -1,22 +1,40 @@
 package myapp.tests.medunna_practice;
 
+import myapp.pages.Medunna_HomePage;
+import myapp.pages.Medunna_RegistrationPage;
+import myapp.utilities.BrowserUtils;
 import myapp.utilities.ConfigReader;
 import myapp.utilities.Driver;
+import myapp.utilities.WaitUtils;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Day21_US001 {
+
+    Medunna_HomePage medunnaHomePage = new Medunna_HomePage();
+    Medunna_RegistrationPage medunnaRegistrationPage = new Medunna_RegistrationPage();
 
 
     @BeforeTest
     public void setUp() {
 //        Driver.getDriver().get("https://www.medunna.com/");
         Driver.getDriver().get(ConfigReader.getProperty("medunna_url"));
+        medunnaHomePage.userIcon.click();
+        WaitUtils.waitFor(2);
+        medunnaHomePage.registerOption.click();
+//        BrowserUtils.clickWithTimeOut(medunnaHomePage.userIcon, 2);
+//        BrowserUtils.clickWithTimeOut(medunnaHomePage.registerOption, 2);
+
     }
 
-    @Test
-    public void medunnaTest() {
 
+    @Test
+    public void medunnaTest1() {
+
+        BrowserUtils.sendKeysWithTimeout(medunnaRegistrationPage.ssn, "22255-5432", 2);
+
+        BrowserUtils.verifyElementDisplayed(medunnaRegistrationPage.invalidSsnMessage);
 
     }
 
